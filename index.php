5fb7de0cea5dbd1c172d7640c5fea5a86d328d87
Agustu55 <gusdstevens@gmail.com>
2020-10-16 17:04:23 -0600

    updated image grid on main

--- index.php
+++ index.php
@@ -25,18 +25,32 @@
       <h4>here are some of my favorites</h4>
       <!-- <?php include 'all_pics.php';?> -->
       <div class="container">
-        <div class="photo-gallery">
-          <img src = https://picsum.photos/300/200 alt="Image 1">
-          <img src = https://picsum.photos/300/200 alt="Image 2">
-          <img src = https://picsum.photos/300/200 alt="Image 3">
-          <img src = https://picsum.photos/300/200 alt="Image 4">
-          <img src = https://picsum.photos/300/200 alt="Image 5">
-          <img src = https://picsum.photos/300/200 alt="Image 6">
-          <img src = https://picsum.photos/300/200 alt="Image 7">
-          <img src = https://picsum.photos/300/200 alt="Image 8">
-          <img src = https://picsum.photos/300/200 alt="Image 9">
-        </div>
+        <img class="img1" src = small-images/1.jpg alt="Image 1">
+        <img class="img2" src = small-images/2.jpg alt="Image 2">
+        <img class="img3" src = small-images/3.jpg alt="Image 3">
+        <img class="img4" src = small-images/4.jpg alt="Image 4">
+        <img class="img5" src = small-images/5.jpg alt="Image 5">
+        <img class="img6" src = small-images/6.jpg alt="Image 6">
+        <img class="img7" src = small-images/7.jpg alt="Image 7">
+        <img class="img8" src = small-images/8.jpg alt="Image 8">
+        <img class="img9" src = small-images/9.jpg alt="Image 9">
       </div>
-    </div>
   </body>
 </html>
+
+
+
+
+
+
+<!-- <div class="container">
+  <div class="img1">1</div>
+  <div class="img2">2</div>
+  <div class="img3">3</div>
+  <div class="img4">4</div>
+  <div class="img5">5</div>
+  <div class="img6">6</div>
+  <div class="img7">7</div>
+  <div class="img8">8</div>
+  <div class="img9">9</div>
+</div> -->
